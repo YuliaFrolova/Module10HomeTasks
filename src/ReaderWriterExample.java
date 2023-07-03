842cc5f54be36b191536e37e639e409421e5a66e
yulia <vaydafox@gmail.com>
2023-07-03 20:04:57 +0300

    Finished first task

--- src/ReaderWriterExample.java
+++ src/ReaderWriterExample.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.util.regex.Pattern;
 
 
 public class ReaderWriterExample {
@@ -17,7 +18,9 @@ public class ReaderWriterExample {
         try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file))){
             bufferedWriter.write("098-322-4456\n");
             bufferedWriter.write("098322-4456\n");
+            bufferedWriter.write("(097) 374-4658\n");
             bufferedWriter.write("(097)-374-4658\n");
+            bufferedWriter.write("786-987-1253");
 
         }catch (IOException e){
             System.err.println(e.getMessage());
@@ -34,8 +37,11 @@ public class ReaderWriterExample {
 
             try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
             String line = reader.readLine();
-            while (line != null){
-                System.out.println(line);
+            while (line != null) {
+                if (line.matches("\\d{3}-\\d{3}-\\d{4}")
+                        || line.matches("\\(\\d{3}\\)\\s\\d{3}-\\d{4}")) {
+                    System.out.println(line);
+                }
                 line = reader.readLine();
             }
             }catch (IOException e){
